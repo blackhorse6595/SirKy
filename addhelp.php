c287abea8f25e0db6c49448acde0c601870ad4eb
wrynnme <yanaworavat@outlook.com>
2019-11-02 02:10:16 +0700

    Update addhelp.php

--- addhelp.php
+++ addhelp.php
@@ -1,31 +1,29 @@
-<?php  
+<?php
 include_once("connect.php");
-// if(isset($_POST['action'])){ 
-//     echo "<script>console.log('1');</script>"
-// }
-// if(isset($_POST['action']) && $_POST['action']=="add"){      
-//     $lat = $_POST["myPosition_lat"];
-//     $lon = $_POST["myPosition_lon"];
-//     $user = $_POST["user"];
-//     $name = $_POST["name"];
-//     $des = $_POST["messagetext"];
-//     // $sql = "INSERT INTO help VALUES(NULL,'$lat','$lon','$user','$name','$des')" ;
-//     // $result = $con->query($sql);
-// }
-// if(isset($_POST['action']) && ($_POST['action'] == "aa")){
-    $lat = $_POST['lat'];
-    $lon = $_POST['lon'];
-    $user = $_POST['user'];
-    $name = $_POST['name'];
-    $data = '1';
-    $sql = "INSERT INTO `help` VALUES(NULL, '$lat','$lon','$user','$name','$data')";
-    $result = $con->query($sql);
-    if($result){
-        echo "success";
-    }else{
-        echo "fail";
-    }
-// }
-  
+SESSTION_START();
+if(isset($_POST['action']) && $_POST['action'] == "1"){
+	$_SESSION['lat'] = $_POST["lat"];
+	$_SESSION['lon'] = $_POST["lon"];
+	echo $_SESSION['lat'];
+	$user = $_POST["user"];
+	$name = $_POST["name"];
+	$des = $_POST["messagetext"];
+	// $sql = "INSERT INTO help VALUES(NULL,'$lat','$lon','$user','$name','$des')" ;
+	// $result = $con->query($sql);
+}
+ if(isset($_POST['action']) && ($_POST['action'] == "2")){
+	$lat = $_POST['lat'];
+	$lon = $_POST['lon'];
+	$user = $_POST['user'];
+	$name = $_POST['name'];
+	$data = '1';
+	$sql = "INSERT INTO `help` VALUES(NULL, '$lat','$lon','$user','$name','$data')";
+	$result = $con->query($sql);
+	if($result){
+		echo "success";
+	}else{
+		echo "fail";
+	}
+}
 exit();
 ?>
