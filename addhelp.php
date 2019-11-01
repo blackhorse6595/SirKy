debe64b1627d22c69c137b1bd303c501c67a1e8a
gieyazka <57186807+gieyazka@users.noreply.github.com>
2019-11-01 13:46:08 +0700

    Revert "Update addhelp.php"
    This reverts commit 373f38a36f02c31691744697b763863eac8a8aa3.

--- addhelp.php
+++ addhelp.php
@@ -1,26 +1,31 @@
-<?php
+<?php  
 include_once("connect.php");
-SESSION_START();
-if(isset($_POST['action']) && ($_POST['action'] == "1")){
-	echo 'Send Lat and Lon';
-	$_SESSION['lat'] = $_POST['lat'];
-	$_SESSION['lon'] = $_POST['lon'];
-}
-if(isset($_POST['action']) && ($_POST['action'] == "2")){
-	echo 'Send Text';
-	$lat = $_SESSION['lat'];
-	$lon = $_SESSION['lon'];
-	$user = $_SESSION['username'];
-	$name = $_SESSION['User'];
-	$des = $_POST["txt"];
-	$sql = "INSERT INTO help VALUES(NULL,'$lat','$lon','$user','$name','$des')" ;
-	$result = $con->query($sql);
-	if ($result) {
-		echo "<br>Insert Success";
-	}else{
-		echo "<br> Error";
-	}
-}
-
+// if(isset($_POST['action'])){ 
+//     echo "<script>console.log('1');</script>"
+// }
+// if(isset($_POST['action']) && $_POST['action']=="add"){      
+//     $lat = $_POST["myPosition_lat"];
+//     $lon = $_POST["myPosition_lon"];
+//     $user = $_POST["user"];
+//     $name = $_POST["name"];
+//     $des = $_POST["messagetext"];
+//     // $sql = "INSERT INTO help VALUES(NULL,'$lat','$lon','$user','$name','$des')" ;
+//     // $result = $con->query($sql);
+// }
+// if(isset($_POST['action']) && ($_POST['action'] == "aa")){
+    $lat = $_POST['lat'];
+    $lon = $_POST['lon'];
+    $user = $_POST['user'];
+    $name = $_POST['name'];
+    $data = '1';
+    $sql = "INSERT INTO `help` VALUES(NULL, '$lat','$lon','$user','$name','$data')";
+    $result = $con->query($sql);
+    if($result){
+        echo "success";
+    }else{
+        echo "fail";
+    }
+// }
+  
 exit();
 ?>
