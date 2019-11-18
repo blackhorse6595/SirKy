3141cf09f87f348e2d7729f3b00d25675d1642af
gieyazka <57186807+gieyazka@users.noreply.github.com>
2019-11-18 23:50:26 +0700

    commit

--- adminindex.php
+++ adminindex.php
@@ -96,7 +96,7 @@
       
       <!-- Nav Item - Tables -->
       <li class="nav-item">
-        <a class="nav-link" href="tables.html">
+        <a class="nav-link" href="emptable.php">
           <i class="fas fa-fw fa-table"></i>
           <span>ดูข้อมูลพนักงาน</span></a>
       </li>
