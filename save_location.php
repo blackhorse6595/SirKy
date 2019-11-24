4b23f1a81ac8b761eb674b7e57b0415700588519
gieyazka <57186807+gieyazka@users.noreply.github.com>
2019-11-24 18:08:13 +0700

    Revert "Revert "พระเจ้าช่วย deadline อยู่แค่เอื้อม""
    This reverts commit 1b5cf6d269a190a8c117d18f8d69998d97c86ebe.

--- save_location.php
+++ save_location.php
@@ -8,9 +8,6 @@ $name = $_REQUEST['name'];
 $sql = "update user_position SET user_latitude='". $lat ."', user_longitude='". $lon ."'  WHERE user_id='". $user ."'";
 $result = $con->query($sql);
 echo $result;
-// ตรวจสอบว่ามีการส่งคำค้นมาหรือไม่
-// ตรวจสอบว่า มีข้อมูล สมาชิก ไอดี นี้อยู่ก่อนหรือไม่ ถ้ามี 
- 
-// ให้อัพเดทข้อมูล  
+
  
 ?>
